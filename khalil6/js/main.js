05cafb16651a013eda79fb0af615e19f44380f42
khalyel05311997 <41632457+khalyel05311997@users.noreply.github.com>
2018-07-26 16:25:26 +0200

    Add files via upload

---
+++ khalil6/js/main.js
@@ -0,0 +1,23 @@
+function affb() {
+
+
+$("#p1").css('fontWeight','900');
+}
+
+function affi() {
+$("#p1").css('fontStyle','italic');
+}
+
+function affu() {
+$("#p1").css('textDecoration','underline');
+}
+
+function Size() {
+	var a=$("#type2").val();
+	$("#p1").css('fontSize', a);
+}
+
+function style1(){
+var a=$("#type1").val();
+$("#p1").css('fontFamily', a);
+}
